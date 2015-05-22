ebd65d7310bcbe915a93d4ef5d306710c40a0382
Alex Emrie <emrieaj@gmail.com>
2015-05-21 21:18:14 -0600

    Completed Update/Destroy actions

--- routes/classrooms.js
+++ routes/classrooms.js
@@ -11,10 +11,116 @@ router.get('/', function(req, res) {
   });
 });
 
+router.post('/', function(req, res) {
+  var name = req.body.name;
+  var seats = req.body.numberOfSeats;
+  var keg = req.body.hasKeg;
+  Classroom.create({
+    name: name,
+    numberOfSeats: seats,
+    hasKeg: keg
+  }, function(err, classroom) {
+    if (err) console.log(err);
+    else {
+      console.log('Post creating new classroom');
+      res.format({
+        html: function() {
+          res.location("classrooms");
+          res.redirect("/classrooms");
+        },
+        json: function() {
+          res.json(classroom);
+        }
+      });
+    }
+  });
+});
+
+router.get('/new', function(req, res) {
+  res.render('classrooms/new', {title: "Let's create a New Classroom yo!"});
+});
+
+router.param('id', function(req, res, next, id) {
+  Classroom.findById(id, function(err, classroom) {
+    if (err) {
+      console.log(id + ' was not found');
+      res.status(404);
+    } else {
+      req.id = id;
+      next();
+    }
+  });
+});
+
+
 router.get('/:id', function(req, res) {
-  Classroom.findOne({_id: req.params.id}, function(err, classroom) {
+  Classroom.findById(req.id, function(err, classroom) {
     res.render('classrooms/show', {classroom: classroom});
   });
 });
+
+router.get('/:id/edit', function(req, res) {
+  Classroom.findById(req.id, function(err, classroom) {
+    if(err) console.log(err);
+    else {
+      res.format({
+        html: function() {
+          res.render('classrooms/edit', {classroom: classroom});
+        },
+        json: function() {
+          res.json(classroom);
+        }
+      });
+    }
+  });
+});
+
+router.post('/:id/edit', function(req, res) {
+  console.log('accessed the put method');
+  var name = req.body.name;
+  var seats = req.body.numberOfSeats;
+  var keg = req.body.hasKeg;
+  Classroom.findById(req.id, function(err, classroom) {
+    classroom.update({
+      name: name,
+      numberOfSeats: seats,
+      hasKeg: keg
+    }, function(err, classroomID) {
+      if (err) res.send("There was an error updating the information");
+      else {
+        res.format({
+          html: function() {
+            console.log("About to update");
+            res.redirect("/classrooms/" + classroom._id);
+          },
+          json: function() {
+            res.json(classroom);
+          }
+        });
+      }
+    });
+  });
+});
+
+router.get('/:id/delete', function(req, res) {
+  console.log("About to be deleted");
+  Classroom.findById(req.id, function(err, classroom) {
+    if (err) console.log(err);
+    else {
+      classroom.remove(function(err, classroom) {
+        console.log('Delete removing: ' + classroom._id);
+        res.format({
+          html: function() {
+            res.redirect("/classrooms");
+          },
+          json: function() {
+            res.json(classroom);
+          }
+        });
+      });
+    }
+  });
+});
+
 
 module.exports = router;
